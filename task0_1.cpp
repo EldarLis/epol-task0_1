a7c7532b66f66bafdba5c97b33e440d6017998ce
EldarLis <eldaarliis@gmail.com>
2020-03-05 21:41:08 +0300

    Update task0_1.cpp

--- task0_1.cpp
+++ task0_1.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
-
 using namespace std;
-void myswap(int* x, int* y);
 
+void myswap(int& x, int& y)
+{
+x = x + y;
+y = x - y;
+x = x - y;
+}
 
 int main()
 {
-int a, b;
-cout << "Enter two integers: ";
-cin >> a >> b;
-myswap(&a, &b);
-cout << a <<" "<< b << endl;
-return 0;
-}
-
-void myswap(int* x, int* y)
-{
-*x = *x + *y;
-*y = *x - *y;
-*x = *x - *y;
+    int a, b;
+    cout << "Enter two ints: ";
+    cin >> a >> b;
+    myswap(a, b);
+    cout << a <<" "<< b << endl;
+    return 0;
 }
